73df01b1adb8522fff869b11e1d84be1aab1f02f
Abel Arismendy <abel@arismendy.co>
2024-09-14 22:53:14 -0500

    widgetbook config

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+import 'package:eco_bites/core/config/theme.dart';
+import 'package:eco_bites/core/utils/create_text_theme.dart';
 
 import 'main.directories.g.dart';
 
@@ -14,8 +16,34 @@ class WidgetbookApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    TextTheme textTheme = createTextTheme(context, "Roboto", "Roboto");
+
+    MaterialTheme theme = MaterialTheme(textTheme);
+
     return Widgetbook.material(
       directories: directories,
+      appBuilder: (context, child) => ColoredBox(
+        color: theme.light().colorScheme.surface,
+        child: child,
+      ),
+      addons: [
+        DeviceFrameAddon(devices: [
+          Devices.android.samsungGalaxyNote20,
+          Devices.ios.iPhone13,
+        ],
+        initialDevice: Devices.android.samsungGalaxyNote20,
+        ),
+        InspectorAddon(),
+        AlignmentAddon(),
+        BuilderAddon(name: 'SafeArea', builder: (_,child) => SafeArea(child: child,))
+      ],
+      integrations: [
+        WidgetbookCloudIntegration(),
+      ],
+      lightTheme: theme.light(),
+      darkTheme: theme.dark(),
+      themeMode: ThemeMode.light,
     );
   }
 }
