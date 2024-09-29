e0db1ef34911954372d04dd209cb70b71118aa3a
Abel Arismendy <abel@arismendy.co>
2024-09-28 22:50:36 -0500

    remove previous carousel placeholder item

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -195,29 +195,3 @@ class _HomeScreenContentState extends State<HomeScreenContent>
     );
   }
 }
-
-class UncontainedLayoutCard extends StatelessWidget {
-  const UncontainedLayoutCard({
-    super.key,
-    required this.index,
-    required this.label,
-  });
-
-  final int index;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return ColoredBox(
-      color: Colors.primaries[index % Colors.primaries.length].withOpacity(0.5),
-      child: Center(
-        child: Text(
-          label,
-          style: const TextStyle(color: Colors.white, fontSize: 20),
-          overflow: TextOverflow.clip,
-          softWrap: false,
-        ),
-      ),
-    );
-  }
-}
