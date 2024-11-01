8835beff7ddf8f333062f9675dfd69553ec356c7
Raul Rincon <r.rincon@uniandes.edu.co>
2024-10-31 23:34:24 -0500

    Feature: Refactor AddressRepository and FetchUserAddressUseCase to return a single address instead of a list

--- lib/features/address/domain/repositories/address_repository.dart
+++ lib/features/address/domain/repositories/address_repository.dart
@@ -1,8 +1,9 @@
+// address_repository.dart
 import 'package:dartz/dartz.dart';
 import 'package:eco_bites/core/error/failures.dart';
 import 'package:eco_bites/features/address/domain/entities/address.dart';
 
 abstract class AddressRepository {
   Future<Either<Failure, void>> saveAddress(String userId, Address address);
-  Future<Either<Failure, List<Address>>> fetchUserAddresses(String userId);
+  Future<Either<Failure, Address?>> fetchUserAddress(String userId);
 }
--- lib/features/address/domain/usecases/fetch_user_addresses_usecase.dart
+++ lib/features/address/domain/usecases/fetch_user_addresses_usecase.dart
@@ -4,18 +4,19 @@ import 'package:eco_bites/features/address/domain/entities/address.dart';
 import 'package:eco_bites/features/address/domain/repositories/address_repository.dart';
 import 'package:equatable/equatable.dart';
 
-class FetchUserAddressesUseCase {
-  const FetchUserAddressesUseCase(this.repository);
+class FetchUserAddressUseCase {
+  const FetchUserAddressUseCase(this.repository);
 
   final AddressRepository repository;
 
-  Future<Either<Failure, List<Address>>> call(FetchUserAddressesParams params) {
-    return repository.fetchUserAddresses(params.userId);
+  // Update to return a single Address, not a list.
+  Future<Either<Failure, Address?>> call(FetchUserAddressParams params) {
+    return repository.fetchUserAddress(params.userId);
   }
 }
 
-class FetchUserAddressesParams extends Equatable {
-  const FetchUserAddressesParams({required this.userId});
+class FetchUserAddressParams extends Equatable {
+  const FetchUserAddressParams({required this.userId});
 
   final String userId;
 
