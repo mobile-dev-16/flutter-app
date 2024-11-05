3fcc3517f70b1bcd6f79833fbddc4a0e84e82823
Abel Arismendy <abel@arismendy.co>
2024-11-04 21:05:21 -0500

    remove widgetbook

--- widgetbook/lib/core/ui/widgets/bottom_navbar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
-
-import 'package:eco_bites/core/ui/widgets/bottom_navbar.dart';
-
-@widgetbook.UseCase(name: 'Default', type: BottomNavbar)
-Widget buildBottomNavbarUseCase(BuildContext context) {
-  return BottomNavbar(currentIndex: 0, onTap: (index) => {});
-}
